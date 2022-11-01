b24bcac588f503efa79259dc4ca6e4f5c2e48307
dorofeevs <aleksandr.dorofeev@akvelon.com>
2022-11-01 11:30:39 +0400

    Initial commit

---
+++ VKSample/LoginViewController.swift
@@ -0,0 +1,13 @@
+// LoginViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Login screen.
+final class LoginViewController: UIViewController {
+    // MARK: - Life cycle.
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
